5d9891f6ed24ecd3dc027268d5c79540b01cb721
maikalangi <maikalangi@gmail.com>
2022-06-17 02:07:21 -0700

    google firebase is live

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
+import { useState, useEffect } from 'react';
+import { auth } from './services/firebase';
 import Nav from './components/Nav';
 import Main from './components/Main';
 import "./styles.scss";
 
 function App() {
+  const [ user, setUser ] = useState(null);
+
+  useEffect (() => {
+    const unsubscribe = auth.onAuthStateChanged(user => setUser(user));
+    return ()=> {
+      unsubscribe();
+    }
+  }, []);
+
+  useEffect(() => {
+    auth.onAuthStateChanged(user => setUser(user))
+  }, []);
+
   return (
     <div className="App">
       <h1>General Store</h1>
-      <Nav />
-      <Main />
+      <Nav user={user} />
+      <Main user={user} />
     </div>
   );
 }
