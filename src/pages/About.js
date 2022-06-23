c5c1105178de287074452d7350fd0cdccf719edc
JQUAN CALEK <jquancollins96@gmail.com>
2022-06-23 11:48:23 -0400

    about_js added

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,34 +1,21 @@
+import aboutData from "../projectabout/About.json";
+import "../App.css";
 
-import { useState, useEffect } from "react";
+const formatData = () => {
+  return aboutData.map((person, index) => {
+    return (
+      <div key={index} className="person_info">
+        <h2>{person.name}</h2>
+        <img src={person.headshot} alt="img" />
+        <p> {person.bio}</p>
+        <p>{person.email}</p>
+      </div>
+    );
+  });
+};
 
 function About(props) {
-  // create state to hold about data
-  const [about, setAbout] = useState(null);
-
-  // create function to make api call
-  const getAboutData = async () => {
-    // make api call and get response
-    const response = await fetch(props.URL + "about");
-    // turn response into javascript object
-    const data = await response.json();
-    // set the about state to the data
-    setAbout(data);
-  };
-
-  // make an initial call for the data inside a useEffect, so it only happens once on component load
-  useEffect(() => {getAboutData()}, []);
-
-  // define a function that will return the JSX needed once we get the data
-  const loaded = () => (
-    <div>
-      <h2>{about.name}</h2>
-      <h3>{about.email}</h3>
-      <p>{about.bio}</p>
-    </div>
-  );
-
-  // if data arrives return the result of loaded, if not, an h1 that says loading
-  return about ? loaded() : <h1>Loading...</h1>;
+  return <div className="us">{formatData()}</div>;
 }
 
 export default About;
